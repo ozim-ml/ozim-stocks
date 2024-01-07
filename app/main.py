65f4a66c1c84067b768ea0a4a73e55a3365cd1ee
ozim-ml <michalo4c@gmail.com>
2024-01-07 15:01:11 +0100

    s

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Request, Query
 from fastapi.responses import HTMLResponse
-from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from io import BytesIO
