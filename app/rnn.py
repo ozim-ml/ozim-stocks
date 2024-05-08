7d46bd409737c7399bc51cf62576755207626042
Michał Ozimek <93000826+ozim-ml@users.noreply.github.com>
2024-05-08 11:25:08 +0200

    Update rnn.py

--- app/rnn.py
+++ app/rnn.py
@@ -1,91 +1,25 @@
 from app.main import *
-from sklearn.preprocessing import MinMaxScaler
-import tensorflow as tf
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout
 
-mlflow.set_experiment("stocks-lstm")
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, InputLayer
+from keras.callbacks import EarlyStopping
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime as dt
+
+# mlflow.set_experiment("stocks-lstm")
 
 def perform_lstm(df, ticker):
 
-    tf.random.set_seed(1)
 
-    df_adj = df.filter(['Adj Close'])
-    lstm_df = df_adj.values
 
-    train_len_val = .80
-    train_len = int(np.ceil(len(lstm_df) * train_len_val))
 
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_data = scaler.fit_transform(lstm_df)
-
-    train_data = scaled_data[0:int(train_len), :]
-
-    x_train, y_train = [],[]
-
-    interval = 60
-
-    for i in range(interval, len(train_data)):
-        x_train.append(train_data[i - interval:i, 0])
-        y_train.append(train_data[i, 0])
-
-    x_train, y_train = np.array(x_train), np.array(y_train)
-
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-    # Create LSTM model
-    model = Sequential()
-    model.add(LSTM(128, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-    model.add(Dropout(0.2))
-    model.add(LSTM(64, return_sequences=False))
-    model.add(Dropout(0.2))
-    model.add(Dense(25))
-    model.add(Dropout(0.2))
-    model.add(Dense(1))
-    model.add(Dropout(0.2))
-
-    model.compile(optimizer='adam', loss='mean_squared_error', metrics='MAE')
-
-    batch_size = 16
-    epochs = 10
-
-    with mlflow.start_run():
-
-        mlflow.tensorflow.autolog()
-
-        model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
-
-        test_data = scaled_data[train_len - interval:, :]
-        x_test = []
-        y_test = lstm_df[train_len:, :]
-
-        for i in range(interval, len(test_data)):
-            x_test.append(test_data[i - interval:i, 0])
-
-        x_test = np.array(x_test)
-        x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-        predictions = model.predict(x_test)
-        predictions = scaler.inverse_transform(predictions)
-
-        mean = np.mean(y_test)
-        rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
-        cv_rmse = rmse/mean
-
-        mlflow.log_metric('CV RMSE', cv_rmse)
-
-    train = df_adj[:train_len]
-    valid = df_adj[train_len:]
-    valid['Predictions'] = predictions
-
-    plt.figure(figsize=(12, 6))
-    plt.title(f'Model of {ticker}')
-    plt.xlabel('Date')
-    plt.ylabel('Adj Close Price')
-    plt.plot(train['Adj Close'])
-    plt.plot(valid[['Adj Close', 'Predictions']])
-    plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
-
+    
     # Create a BytesIO object to store the plot
     plot_bytes_lstm = BytesIO()
 
